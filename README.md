f8c4aabaabeba27e296ab29bbb3fb142e3f90c47
cagedev <cagedev@gmail.com>
2022-04-22 16:56:31 +0200

    Many little updates; not working yet.

--- README.md
+++ README.md
@@ -2,25 +2,101 @@
 Javascript library for synchronising an HTML form field across clients.
 
 ## Usage
-```
+```shell
 npm run dev
 ```
 
 ## Configuration / Protocol
 
-The client must be configured with the following settings
-```
+The client must be configured with the following settings:
+```js
 wfsConfig = {
-    serverUrl: "", // server url - websocket connection url; local
-    userName: "", // user nickname - for public display; broadcast
-    userId: "", // user id - for identification etc.; broadcast
-    roomId: "", // room id - for grouping; broadcast
-    eventElementId: "", // local id of the DOM element that triggers the update event; local
-    updateEvent: "", // event used for sending an update message; local
-    syncType: "", // SEND (broadcast) (type of synchronisation between clients; broadcast)
-    dataFieldId: "", // local id of DOM element containing the linked data (local)
+    serverUrl: "",      // server url - websocket connection url; local
+    userName: "",       // user nickname - for public display; broadcast
+    userId: "",         // user id - for identification etc.; broadcast
+    connections:        // array of requested connections; each connection is initiated seperately (?)
+        [
+            {
+                roomId: "",         // room id - for grouping; broadcast; defaults to null
+                eventElementId: "", // DOM id of the DOM element that triggers the update event; local
+                updateEvent: "",    // event used for sending an update message; local
+                syncType: "",       // type of synchronisation between clients; broadcast; defaults to "SEND"
+                dataFieldId: "",    // local id of DOM element containing the linked data (local)
+            }
+        ]
 }
 ```
+
+### Example usage:
+```html
+<form>
+    <textarea id="myDataField"></textarea>
+</form>
+```
+
+```js
+var wfs = new WfsConnection(config = {
+    serverUrl: "ws://127.0.0.1:8080",
+    userName: "Test Nickname",
+    userId: "00000000-0000-0000-0000-000000000000",
+    connections: 
+        [
+            {    
+                roomId: null,
+                dataElementId: "myDataField",
+                updateEvent: "onchange",
+                eventElementId: "myDataField",
+                syncType: "SEND",
+            }   
+        ]
+});
+```
+
+## Message Protocol
+
+The JSON messages consist of a String `event`-flag and a `payload`-object.
+
+For example:
+```json
+message = {
+    "event": "RESPONSE",
+    "wfsData": {
+        // wfsData-object
+    },
+    "payload": {
+        "type": "UPDATE",
+        "var":  "ROOM_ID",
+        "value": roomId,
+    }
+}
+```
+
+Current implementation:
+
+```json
+wfsData = {
+    "roomId"   = wfsConfig.roomId;
+    "syncType" = wfsConfig.syncType;
+    "userId"   = wfsConfig.userId;
+    "userName" = wfsConfig.userName;
+}
+```
+
+Server pushes `'UPDATE'` messages in response to (websocket) events or (`'SERVER_REQUEST'`) client messages. Client and server exchange actual sync data with the `'DATA_MESSAGE'`. Client-to-server messages are validated; server responds with an appropriate error. Client 
+
+| Event            | Payload                                                                                    |  roomId  | syncType |  userId  | userName |
+| :--------------- | :----------------------------------------------------------------------------------------- | :------: | :------: | :------: | :------: |
+| `UPDATE`         | `state` : String                                                                           |          | required |          | optional |
+| `SERVER_REQUEST` | `requestedData : String ` from `["USER_ID", "ROOM_ID", "MESSAGE_HISTORY"]`                 |          | required |          | optional |
+|                  | `requestedData = "USER_ID"`                                                                |          | required |          | optional |
+|                  | `requestedData = "ROOM_ID"`                                                                |          | required | required | optional |
+|                  | `requestedData = "MESSAGE_HISTORY"`                                                        | required | required | required | optional |
+| `DATA_MESSAGE`   | `messageData : String \| JSON`                                                             | required | required | required | optional |
+| `RESPONSE`       | `type : String` from `["UPDATE"]`,                                                         |   none   |   none   |   none   |   none   |
+|                  | `var : String` <variable_name> from `[ "STATE", "USER_ID", "ROOM_ID", "MESSAGE_HISTORY" ]` |          |          |          |          |
+|                  | `value : String \| Object` <variable_value>                                                |          |          |          |          |
+| `ERROR`          | `type : String` from `["INVALID_VARIABLE", "MALFORMED_REQUEST"]`                           |   none   |   none   |   none   |   none   |
+|                  | `var : String` <variable_name> from `[ "USER_ID", "ROOM_ID" ]`                             |          |          |          |          |
 
 ## TODO
  - [x] Basic websocket echo-to-all
@@ -28,4 +104,7 @@ wfsConfig = {
  - [x] Working proof of concept (simple client)
  - [ ] Working proof of concept (overview client)
  - [ ] Abstract client-side code to a library
- - [ ] 
+ - [ ] Message protocol
+   - [ ] Streamline different message types
+ - [ ] Style guide
+ - [ ] Rename to `EntangleJS`
--- server/server.js
+++ server/server.js
@@ -1,16 +1,106 @@
 import { WebSocketServer } from 'ws';
 import { NIL as uuid_NIL } from 'uuid';
+import { v4 as uuid_v4 } from 'uuid';
+console.log(uuid_NIL);
+import * as yup from 'yup';
+// import { * as yup } from 'yup';
 
 // import { Dirty } from 'dirty';
 import pkg from 'dirty';
 const { Dirty } = pkg;
 
+// TODO Design database schema
+// TODO? Include in JWT?
+// TODO class UserDB
+ // check_password()
+ // generate_token()
+ // get_roomIds()
+// var users = new Dirty('users.json');
+// db.on('load', function (length) {
+//     console.log(length, 'users in database.');
+// });
+
+// var userSchemaJSON = {
+//     key: userId,
+//     value: {
+//         name: "Display Name",
+//         roomIds:
+//             [
+//                 "",
+//             ],
+//         permissions:
+//         {
+//             createNewRoom: true,
+//             maxRooms: 1,
+//         },
+//         lastSeen: "" // DateTime of last update
+//     }
+// }
+
+
+// TODO Databse cleanup: 
+// - [ ] seperate users.db for permissions (probably bad)
+// - [ ] save room data 
+// - [ ] cleanup on exit
 var db = new Dirty('db.json');
 
 db.on('load', function (length) {
     console.log('Currently', length, 'records.');
-    db.set('testKey', { data: "something" });
+    // DEBUG
+    // db.set('testKey', { data: "something" });
 });
+
+/**
+ * Yup websocket message validation schemas
+ * 
+ * @type {Object.<string, yup.Schema, yup.Schema>}
+ */
+const messageSchema = yup.object({
+    event: yup.string().required().oneOf(
+        ["SERVER_REQUEST", "DATA_MESSAGE"]
+    ), // from list
+    wfsData: yup.object().shape({
+        userName: yup.string(),
+        userId: yup.string().test(
+            'isUUID',
+            "Invalid value for ${path} (${value})",
+            function (value) { return isUUID(value) },
+        ),
+        roomId: yup.string().required(),
+        syncType: yup.string(), // from list
+    }),
+    payload: yup.object(),
+});
+
+
+function isUUID(uuid) {
+    // console.log(uuid);
+    // if (uuid == uuid_NIL) {
+    //     return true;
+    // }
+
+    let s = "" + uuid;
+    s = s.match('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$');
+    if (s === null) {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Parses JSON message string and validates against the expected schema.
+ * 
+ * @param {string} messageString 
+ * @returns {{event: string, wfsData: object, payload: object}}
+ * @throws Will throw and error if the message is invalid JSON or and invalid schema.
+ */
+function parseMessage(messageString) {
+    let msg;
+    msg = JSON.parse(messageString);
+    msg = messageSchema.validateSync(msg);
+    return msg;
+}
 
 
 // const connections = {} // (rooms -> users)
@@ -28,13 +118,9 @@ wss.on('connection', function connection(ws) {
 
     ws.on('message', function (data) {
 
-        // TODO incoming message format ({event:<string>,payload:<object>}?)
-        // TODO validate incoming message (yupjs)
-
-        // Check message type (try?)
         let msg;
         try {
-            msg = JSON.parse(data.toString());
+            msg = parseMessage(data);
         }
         catch (e) {
             console.log("Error:", e.message);
@@ -42,15 +128,15 @@ wss.on('connection', function connection(ws) {
         }
 
 
-
         if (msg.event == 'SERVER_REQUEST') {
             // handle request
             if (msg.payload.requestedData == 'USER_ID') {
                 // generate id
-                let userId = uuid_NIL;
+                // let userId = uuid_NIL;
+                let userId = uuid_v4();
                 // set messages list for id to empty array
                 db.set(userId, JSON.stringify(
-                    { history: [] }
+                    { history: [], roomIds: [] } // also empty array for roomIds?
                 ))
                 // send RESPONSE
                 ws.send(
@@ -63,15 +149,69 @@ wss.on('connection', function connection(ws) {
                         }
                     })
                 );
+            } else if (msg.payload.requestedData == 'ROOM_ID') {
+                if (msg.wfsData.userId) {
+                    console.log(msg.wfsData.userId);
+
+                    let usr = db.get(msg.wfsData.userId);
+                    try {
+                        usr = JSON.parse(usr); // try?
+                    }
+                    catch (e) {
+                        console.log("Error:", e.message);
+                        return null; // ??
+                    }
+
+                    // If userId exists in database
+                    if (usr) {
+                        // Y: check if user has existing room(s)
+
+                        // Y: return rooms
+                        // N: generate roomId
+                        // set roomId
+                        // return roomId  
+                        // generate id
+                        let roomId = uuid_v4();
+
+                        db.update(
+                            msg.wfsData.userId, function (value) {
+                                let val = JSON.parse(value);
+                                val.roomIds.push(roomId);
+                                console.log(val);
+                                return JSON.stringify(val);
+                            }
+                        );
+
+                        // send RESPONSE
+                        ws.send(
+                            JSON.stringify({
+                                event: "RESPONSE",
+                                payload: {
+                                    type: "UPDATE",
+                                    var: "ROOM_ID",
+                                    value: roomId,
+                                }
+                            })
+                        );
+                    } else {
+                        console.log("[Error] userId does not exist", msg.wfsData.userId);
+                    }
+                } else {
+                    console.log("[Error] userId missing:", msg.wfsData);
+                }
+
+            }
+            else {
+                console.log("[Error] Unrecognized SERVER_REQUEST:", msg.payload.requestedData)
             }
         }
-        else if (msg.type = 'MESSAGE') {
+        else if (msg.type = 'DATA_MESSAGE') {
 
             // DEBUG
-            console.log('MESSAGE', msg.payload);
+            console.log('DATA_MESSAGE', msg.payload);
 
             // Log message to database
-            // TODO: 
+            // TODO: Check if userId is in the active connections pool
             if (msg.wfsData.userId) {
                 db.update(
                     msg.wfsData.userId, function (value) {
@@ -80,7 +220,7 @@ wss.on('connection', function connection(ws) {
                         console.log(val);
                         return JSON.stringify(val);
                     }
-                )
+                );
             }
             // db.set('message', data.toString());
 
